03fd1588ee6639a81589bee327cc8d6d74d21e80
alanjarackal <alanjarackal@gmail.com>
2025-08-23 00:48:58 +0530

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ThaniLocal/',  // Updated to match repository name
   server: {
     port: 8000,
     open: true
